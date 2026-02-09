1b0189be31bd0c87c332e91041661da088b27904
Felix Arnold <felix.arnold@h-partners.com>
2026-02-09 11:03:36 +0100

    Update your module name

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 `default_nettype none
-module tt_um_vga_example (
+module tt_um_farnold_vga1 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
